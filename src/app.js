3c7d804adc51e756df7d24af181dc16fc1bb72bd
Rosario Alonso <79224753+margarita1973@users.noreply.github.com>
2024-10-01 18:28:09 +0000

    margarita1973- Random Card generator
    La función newCard crea una nueva carta cada vez que se recarga la página o al hacer click en el botón Nueva carta.
    La función resizeCard permite cambiar el tamaño de la carta según los parametros ingresados por el usuario

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let cardNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+let cardSuits = ["♦", "♥", "♠", "♣"];
+function randomGenerator(Array) {
+  return Math.floor(Math.random() * Array.length);
+}
+function newcard() {
+  const randomCardNumber = cardNumbers[randomGenerator(cardNumbers)];
+  const randomCardSuits = cardSuits[randomGenerator(cardSuits)];
+  let suitTop = document.getElementById("suit-top");
+  let suitBottom = document.getElementById("suit-bottom");
+  let cardNumber = document.getElementById("card-number");
+
+  cardNumber.innerHTML = randomCardNumber;
+  suitBottom.innerText = randomCardSuits;
+  suitTop.innerText = randomCardSuits;
+  let card = document.querySelector(".card");
+  card.className =
+    randomCardSuits == "♦" || randomCardSuits == "♥"
+      ? "card poker-card-red"
+      : "card poker-card-black";
+}
+
+const addNewCardButtom = document.getElementById("newcard");
+addNewCardButtom.addEventListener("click", newcard);
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newcard();
 };
+
+function resizeCard() {
+  const cardHeight = document.getElementById("cardHeight");
+  const cardWidth = document.getElementById("cardWidth");
+  let card = document.querySelector(".card");
+  card.style.width = cardWidth.value + "px";
+  card.style.height = cardHeight.value + "px";
+  cardHeight.value = "";
+  cardWidth.value = "";
+}
+
+const resizeButton = document.getElementById("resizebutton");
+resizeButton.addEventListener("click", resizeCard);
